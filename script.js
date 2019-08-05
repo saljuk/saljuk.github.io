dcf5edd6ca61148323fb0362a68bb9ea98ab801a
saljuk <51914023+saljuk@users.noreply.github.com>
2019-08-05 16:03:13 +0500

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById("mobilebar").addEventListener("click", function () {
-    var x = document.getElementById("mobilelinks");
-    if (x.style.display === "block") {
-      x.style.display = "none";
-    } else {
-      x.style.display = "block";
-    }
-  }, false);
